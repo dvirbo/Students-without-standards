c5adc6c2e8d53612857c9246c3e320e4bec518ac
Alberto Urbina <49090252+albertoue93@users.noreply.github.com>
2021-05-14 19:56:12 -0600

    funciona

--- app/src/main/java/com/smallacademy/userroles/Register.java
+++ app/src/main/java/com/smallacademy/userroles/Register.java
@@ -1,20 +1,42 @@
 package com.smallacademy.userroles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     EditText fullName,email,password,phone;
     Button registerBtn,goToLogin;
     boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    CheckBox isTeacherBox,isStudentBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         fullName = findViewById(R.id.registerName);
         email = findViewById(R.id.registerEmail);
@@ -23,10 +45,60 @@ public class Register extends AppCompatActivity {
         registerBtn = findViewById(R.id.registerBtn);
         goToLogin = findViewById(R.id.gotoLogin);
 
-        checkField(fullName);
-        checkField(email);
-        checkField(password);
-        checkField(phone);
+        isTeacherBox = findViewById(R.id.isTeacher);
+        isStudentBox = findViewById(R.id.isStudent);
+
+        //
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(fullName);
+                checkField(email);
+                checkField(password);
+                checkField(phone);
+
+                //checkbox confirmation
+                if (!(isTeacherBox.isChecked() || isStudentBox.isChecked())){
+                    Toast.makeText(Register.this,"Select the Account Type",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (valid){
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            Toast.makeText(Register.this,"Account Created",Toast.LENGTH_SHORT).show();
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+                            Map<String,Object> userInfo = new HashMap<>();
+                            userInfo.put("FullName",fullName.getText().toString());
+                            userInfo.put("UserEmail",email.getText().toString());
+                            userInfo.put("PhoneNumber",phone.getText().toString());
+                            //specify admin
+                            userInfo.put("isUser","1");
+
+                            df.set(userInfo);
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Register.this,"Failed to Create Account",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
 
     }
 
