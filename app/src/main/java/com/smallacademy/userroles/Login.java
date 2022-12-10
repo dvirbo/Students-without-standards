9f8df94b7c9afcbc53ee5fc3b9728bd3164ab9a9
Dvir.Bo <dvir.boeochov@gmail.com>
2022-12-10 18:34:29 +0200

    18:34

--- app/src/main/java/com/smallacademy/userroles/Login.java
+++ app/src/main/java/com/smallacademy/userroles/Login.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,7 +15,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
